36a1a5e9b857c2b98515a102b108a8a26153e1fc
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-07-04 14:23:31 -0400

    All tests passing

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    count = 0
+    while count < 100:
+        count += 1
+        if (count % 3 == 0) and (count % 5 == 0):
+            print("FizzBuzz")
+        elif count % 3 == 0:
+            print("Fizz")
+        elif count % 5 == 0:
+            print ("Buzz")            
+        else:
+            print(count)
